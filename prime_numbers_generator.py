dc8da8207f97bbc47d49b723e7170fa0cbf24224
Vladimir Trofimenko <psdmaster.ru@gmail.com>
2021-03-12 10:53:04 +0300

    add function generating prime numbers

--- prime_numbers_generator.py
+++ prime_numbers_generator.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
 
+# функция генерации списка простых чисел
+# def get_prime_numbers(n):
+#     prime_numbers = []
+#     for number in range(2, n + 1):
+#         # проходим по списку уже добавленных простых чисел
+#         for prime in prime_numbers:
+#             if number % prime == 0:  # Если делится без остатка, то значит не наше число
+#                 break
+#         else:  # если не было break
+#             prime_numbers.append(number)
+#     return prime_numbers
+
 # Функция генератор, которая выдает последовательость простых чисел
 def prime_numbers_generator(n):
     prime_numbers = []
